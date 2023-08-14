11441841f5c37e5647daf7428691a1a09c84d648
Javier Estraviz <javier.estraviz@gmail.com>
2023-08-14 19:04:37 +0200

    Used built-in template tags and control structure

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.index),
+    path('home/', views.home, name='home'),
+    path('topstories', views.topstories, name='topstories'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,35 @@ from django.shortcuts import render
 
 # https://docs.djangoproject.com/en/4.2/topics/http/views/
 
+links = [
+    {"name": "Home", "path": "/"},
+    {"name": "Top Stories", "path": "/topstories"},
+    {"name": "Most Popular", "path": "/popular"},
+    {"name": "Real Time Feed", "path": "/feed"}
+]
 
-def index(request):
-    return render(request, 'articles/home.html', {'title': 'Daily News!'})
+
+def home(request):
+    apis = [
+        {"name": "Top Stories API", "path": "/topstories"},
+        {"name": "Most Popular API", "path": "/popular"},
+        {"name": "Times Wire API", "path": "/feed"},
+    ]
+    return render(
+        request,
+        'articles/home.html',
+        {'title': 'Daily News!', "apis": apis}
+    )
+
+
+def topstories(request):
+    return render(
+        request,
+        'articles/results.html',
+        {
+            'title': 'Daily News!',
+            "subtitle": "Top Stories",
+            "results": [],
+            "links": links
+        }
+    )
